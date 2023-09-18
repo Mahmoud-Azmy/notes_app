3688b507e2fa1b8cdc470e8175d5cb2953af0939
Mahmoud-Azmy <mahmoud.azmyali1902@gmail.com>
2023-09-18 19:56:50 +0200

    finish notes bottomSheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:note_app/widgets/custom_bottom.dart';
+
 import 'package:note_app/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -9,19 +11,28 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.only(top: 38, right: 24, left: 24),
-      child: Column(
-        children: [
-          CustomTextField(
-            text: 'Title',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            text: 'Content',
-            maxLines: 5,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            CustomTextField(
+              text: 'Title',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              text: 'Content',
+              maxLines: 5,
+            ),
+            SizedBox(
+              height: 48,
+            ),
+            CustomBottom(),
+            SizedBox(
+              height: 24,
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/custom_bottom.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/const.dart';
+
+class CustomBottom extends StatelessWidget {
+  const CustomBottom({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      padding: EdgeInsets.zero,
+      onPressed: () {},
+      child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: 55,
+          decoration: BoxDecoration(
+              color: kPrimaryColor, borderRadius: BorderRadius.circular(16)),
+          child: const Center(
+            child: Text(
+              'Add',
+              style: TextStyle(color: Colors.black, fontSize: 22),
+            ),
+          )),
+    );
+  }
+}

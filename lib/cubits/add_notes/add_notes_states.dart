96e5f5ade9cb3ab781a6a81cb9a4ec7bc1367c87
Mahmoud-Azmy <mahmoud.azmyali1902@gmail.com>
2023-09-19 11:32:11 +0200

    add AddNotesStates

--- lib/cubits/add_notes/add_notes_states.dart
+++ lib/cubits/add_notes/add_notes_states.dart
@@ -1,3 +1,12 @@
 abstract class AddNotesStates {}
 
 class AddNotesInitialState extends AddNotesStates {}
+
+class AddNotesLoadingState extends AddNotesStates {}
+
+class AddNotesSuccessState extends AddNotesStates {}
+
+class AddNotesfailureState extends AddNotesStates {
+  final String error;
+  AddNotesfailureState(this.error);
+}
